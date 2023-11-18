7f2464e88c9712aad5a186355198bf8287697ff7
Ulisses Mesquita - XPOn <souza.mesquita@gmail.com>
2023-11-18 17:53:30 -0300

    Finalizar leitura

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,12 +3,20 @@ import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
+import java.util.Scanner
 
 
 fun main(args: Array<String>) {
+
+
+    val leitura = Scanner(System.`in`);
+    println("Escolha o proximo id: ")
+    leitura.nextLine()
+    val endereco = "https://www.cheapshark.com/api/1.0/games?id=$leitura";
+
     val client: HttpClient = HttpClient.newHttpClient()
     val request = HttpRequest.newBuilder()
-        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
+        .uri(URI.create(endereco))
         .build();
 
     val response = client
